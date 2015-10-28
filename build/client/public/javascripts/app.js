3cd4cd0d67501439cc3d1354697be9c8a89680ec
root <root@tcb10068.tcloud.infra>
2015-10-28 09:31:53 +0900

    build update

---
+++ build/client/public/javascripts/app.js
@@ -0,0 +1,237 @@
+(function() {
+  'use strict';
+
+  var globals = typeof window === 'undefined' ? global : window;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = ({}).hasOwnProperty;
+
+  var endsWith = function(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+  };
+
+  var _cmp = 'components/';
+  var unalias = function(alias, loaderPath) {
+    var start = 0;
+    if (loaderPath) {
+      if (loaderPath.indexOf(_cmp) === 0) {
+        start = _cmp.length;
+      }
+      if (loaderPath.indexOf('/', start) > 0) {
+        loaderPath = loaderPath.substring(start, loaderPath.indexOf('/', start));
+      }
+    }
+    var result = aliases[alias + '/index.js'] || aliases[loaderPath + '/deps/' + alias + '/index.js'];
+    if (result) {
+      return _cmp + result.substring(0, result.length - '.js'.length);
+    }
+    return alias;
+  };
+
+  var _reg = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (_reg.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var require = function(name, loaderPath) {
+    var path = expand(name, '.');
+    if (loaderPath == null) loaderPath = '/';
+    path = unalias(name, loaderPath);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has.call(cache, dirIndex)) return cache[dirIndex].exports;
+    if (has.call(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '" from '+ '"' + loaderPath + '"');
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          modules[key] = bundle[key];
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+    }
+  };
+
+  require.list = function() {
+    var result = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  };
+
+  require.brunch = true;
+  require._cache = cache;
+  globals.require = require;
+})();
+require.register("application", function(exports, require, module) {
+'use strict';
+angular.module('MyApp', ['ngRoute']).config([
+  '$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+    $httpProvider.defaults.transformRequest = function(poData) {
+      var key, str;
+      str = [];
+      for (key in poData) {
+        str.push(encodeURIComponent(key) + '=' + encodeURIComponent(poData[key]));
+      }
+      return str.join('&');
+    };
+    return $routeProvider.when('/', {
+      templateUrl: 'templates/home',
+      controller: 'MainController'
+    });
+  }
+]).controller('MainController', [
+  '$scope', '$http', function($scope, $http) {
+    $scope.msg = 'hello world!';
+    $scope.bookmarks = [];
+    $scope.bookmarkName = '';
+    $scope.bookmarkURL = '';
+    $scope.addBookmark = function() {
+      return $http({
+        url: 'bookmark',
+        method: 'post',
+        data: {
+          title: $scope.bookmarkName,
+          url: $scope.bookmarkURL
+        }
+      }).success(function(reponseData) {
+        return $scope.getBookmarks();
+      });
+    };
+    $scope.removeBookmark = function(poBookmark) {
+      return $http({
+        url: 'bookmark/' + poBookmark.id,
+        method: 'post'
+      }).success(function(reponseData) {
+        return $scope.getBookmarks();
+      });
+    };
+    $scope.getBookmarks = function() {
+      return $http({
+        url: 'bookmark',
+        method: 'get'
+      }).success(function(responseData) {
+        return $scope.bookmarks = responseData;
+      });
+    };
+    return $scope.getBookmarks();
+  }
+]);
+
+});
+
+require.register("initialize", function(exports, require, module) {
+var app;
+
+app = require('application');
+
+window.onerror = function(msg, url, line, col, error) {
+  var data, exception, xhr;
+  console.error(msg, url, line, col, error, error != null ? error.stack : void 0);
+  exception = (error != null ? error.toString() : void 0) || msg;
+  if (exception !== window.lastError) {
+    data = {
+      data: {
+        type: 'error',
+        error: {
+          msg: msg,
+          name: error != null ? error.name : void 0,
+          full: exception,
+          stack: error != null ? error.stack : void 0
+        },
+        url: url,
+        line: line,
+        col: col,
+        href: window.location.href
+      }
+    };
+    xhr = new XMLHttpRequest();
+    xhr.open('POST', 'log', true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify(data));
+    return window.lastError = exception;
+  }
+};
+
+$(function() {
+  var data, e, error1, exception, xhr;
+  try {
+    return jQuery.event.props.push('dataTransfer');
+  } catch (error1) {
+    e = error1;
+    console.error(e, e != null ? e.stack : void 0);
+    exception = e.toString();
+    if (exception !== window.lastError) {
+      data = {
+        data: {
+          type: 'error',
+          error: {
+            msg: e.message,
+            name: e != null ? e.name : void 0,
+            full: exception,
+            stack: e != null ? e.stack : void 0
+          },
+          file: e != null ? e.fileName : void 0,
+          line: e != null ? e.lineNumber : void 0,
+          col: e != null ? e.columnNumber : void 0,
+          href: window.location.href
+        }
+      };
+      xhr = new XMLHttpRequest();
+      xhr.open('POST', 'log', true);
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhr.send(JSON.stringify(data));
+      return window.lastError = exception;
+    }
+  }
+});
+
+});
+
